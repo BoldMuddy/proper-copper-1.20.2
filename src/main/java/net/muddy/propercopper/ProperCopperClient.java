a6bede7a86dfa9fdc44dec61de70a4de4b69620c
Muddy <121691800+BoldMuddy@users.noreply.github.com>
2023-10-22 20:45:37 +0300

    Initial commit

---
+++ src/main/java/net/muddy/propercopper/ProperCopperClient.java
@@ -0,0 +1,10 @@
+package net.muddy.propercopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProperCopperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/muddy/propercopper/ProperCopperDataGenerator.java
@@ -0,0 +1,11 @@
+package net.muddy.propercopper;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProperCopperDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
